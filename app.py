3bb431db1b27e07f1b48a14c7e0dc1a26cdb36f6
Dzinaishe Mpini <dzinampini@gmail.com>
2025-04-09 17:00:41 +0200

    ADDED: port

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def predict():
         return jsonify({"success": True, "result": prediction_result}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=8080)
